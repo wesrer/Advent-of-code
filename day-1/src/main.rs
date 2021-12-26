6a00062234a1bdba06f6238a4b922181df2d8510
Sayak Chatterjee <Sayak.Chatterjee@tufts.edu>
2021-12-25 23:17:09 -0500

    day-1 problem-2 solved

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -6,14 +6,25 @@ use std::{
 const FILENAME: &str = "input.txt";
 
 fn main() {
+    let input_arr = read_numbers_from_input_file();
+
+    let part_1_answer = part_1(input_arr.clone());
+    let part_2_answer = part_2(input_arr.clone());
+
+    println!("part 1 answer: {}", part_1_answer);
+    println!("part 2 answer: {}", part_2_answer);
+}
+
+fn read_numbers_from_input_file() -> Vec<u64> {
     let file = File::open(FILENAME).expect("couldn't open file");
     let lines = BufReader::new(file).lines();
 
-    let input_arr: Vec<u64> = lines
+    lines
         .map(|line| line.unwrap().parse::<u64>().unwrap())
-        .collect();
+        .collect()
+}
 
-    println!("{}", input_arr.len());
+fn part_1(input_arr: Vec<u64>) -> u64 {
     let mut counter = 0;
 
     input_arr.iter().reduce(|acc, val| {
@@ -22,6 +33,21 @@ fn main() {
         }
         val
     });
+    counter
+}
 
-    println!("{}", counter);
+fn part_2(input_arr: Vec<u64>) -> u64 {
+    let mut counter = 0;
+
+    input_arr.iter().enumerate().fold(0, |mut acc, (i, val)| {
+        if i + 2 < (input_arr.len()) {
+            let current_window = val + input_arr[i + 1] + input_arr[i + 2];
+            if (current_window > acc) && (acc != 0) {
+                counter += 1;
+            }
+            acc = current_window;
+        }
+        acc
+    });
+    counter
 }
